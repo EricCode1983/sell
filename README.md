3bfa1f06917031df8e17dc21fc3e1669e1f350f0
eric <eric@bblab.biz>
2019-02-20 15:36:32 +0800

    5-4 买家商品-api（下）

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+SPRING BOOT+ JPA+MYSQL
+--pending task
+auto gernate table use yaml
+
+Controller->Service (Interface)->ServiceImpl(class,@Service)->Repository (Interface extends Jpa)->DAO(database object entity @Entity)->database
+return view to controller<-
+
+
 1 Spring Initilazer
 2 Log - Logback+SLF4j  (JUL,JCL,Log4j,SLF4j,jobss-logging)
-3 org.projectlombok
-4 @DynamicUpdate with  (on update current_timestamp comment-mysql) can auto update the edit time
-5
+3 org.projectlombok -  (@Data,@Get) not need write getter,setter and tostring function
+4 @DynamicUpdate with  (on update current_timestamp comment: mysql scripts) can auto update the create time and edit time
+5 @Transactional for test , will not impact the database. For example : after you add data to database then will auto detete it
+6 @Service
+7 Chrom plugin to easy check json data jsonview
+8 @JsonProperty 解决前端Json的命名和后端不一致
+9 BeanUtils.copyProperties 用在 daoObject to viewObject or viewObject to daoObject
+
--- src/main/java/com/imooc/dataobject/ProductCategory.java
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Date;
 
 /**
  * 类目
@@ -29,50 +28,6 @@ public class ProductCategory {
 
     /** 类目编号. */
     private Integer categoryType;
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    private Date createTime;
-
-    private Date updateTime;
 
     public ProductCategory() {
     }
